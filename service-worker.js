b63d2515d17099b92f056304913f033983d89e31
vijaydiwancdac <vijaydiwancdac@gmail.com>
2026-05-05 17:27:17 +0530

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "discipline-app-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
